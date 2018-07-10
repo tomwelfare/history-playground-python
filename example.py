5d79a53fc78c6ab2c8d42580b74c80cfda35c7f0
Tom Lansdall-Welfare <tomwelfare@users.noreply.github.com>
2018-07-10 22:31:07 +0100

    Update example.py

--- example.py
+++ example.py
@@ -6,11 +6,10 @@ plygrd = histplygrd.Playground()
 plygrd.login('email','password')
 
 # Select some queries and datasets
-queries = ['dog','cat']
-datasets = plygrd.available_datasets()
+settings = (['dog','cat'], plygrd.available_datasets())
 
 # Query the playground
-series = plygrd.query(queries, datasets)
+series = plygrd.query(settings)
 
 ## (Optional) Plot the results
 util.plot_all_series(series)
